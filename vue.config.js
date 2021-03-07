ec158f894e0bbb7e2c7e4dba7450f337c475b6e9
Jonathan A <jonathanxl1@hotmail.com>
2021-03-06 21:31:03 -0500

    'Support'

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: process.env.NODE_ENV === 'production'
+  "/": process.env.NODE_ENV === 'production'
     ? '/Metafy/'
     : '/'
 };
