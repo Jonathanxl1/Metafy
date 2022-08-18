2bd97c695452fe9b6bd0bb9e84c21dc28b0af0fd
Jonathan <Jonathanxl1@hotmail.com>
2022-08-18 13:53:18 -0500

    Creatd loading overlay WHEN created component

---
+++ src/views/loadingPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card max-width="100%" width="100%">
+    <v-overlay :value="overlay" dark>
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "loadingPage",
+  props: {
+    overlay: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style></style>
